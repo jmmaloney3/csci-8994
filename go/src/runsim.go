5fe2c4bb7684faf5b7c9607df7c3e7094123f153
John Maloney <jmmaloney3@gmail.com>
2016-03-17 18:23:07 -0500

    added command line parsing

--- go/src/runsim.go
+++ go/src/runsim.go
@@ -1,11 +1,33 @@
 package main
 
-import "agent"
+import "sim"
 import "fmt"
+import "flag"
 
+/*
+Run the simulation with the specified arguments.
+
+Arguments:
+  tribes  - number of tribes
+  agents  - number of agents per tribe
+  cost    - cost c to donate
+  benefit - benefit b received from donation
+
+Author: John Maloney
+*/
 func main() {
-  fmt.Println("create simulation")
-  var sim agent.SimEngine = agent.MakeSimEngine(2,2)
-  var p = sim.PlayRounds(1,3)
-  fmt.Println(" payout: ", p)
+  // parse command line arguments
+  numTribes := flag.Int("t", 64, "number f tribes")
+  numAgents := flag.Int("a", 64, "number of agents")
+  cost := flag.Int("c", 1, "cost c to donate")
+  benefit := flag.Int("b", 3, "benefit b received from donation")
+  gens := flag.Int("g", 10, "number of generations to simulate")
+  flag.Parse()
+
+  // run simulation
+  var s sim.SimEngine = sim.MakeSimEngine(*numTribes,*numAgents)
+  for g := 0; g < *gens; g++ {
+    var p = s.PlayRounds(int32(*cost),int32(*benefit))
+    fmt.Println("total payout for generation", g, ": ", p)
+  }
 }
