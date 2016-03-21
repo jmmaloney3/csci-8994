d0c86d77d56064539f3adccf51319f4c286174a1
John Maloney <jmmaloney3@gmail.com>
2016-03-20 23:07:53 -0500

    add support for execution error

--- go/src/sim/action.go
+++ go/src/sim/action.go
@@ -2,24 +2,33 @@ package sim
 
 type ActionModule struct {
   bits [4]bool
+  errP float32 // mu_e - execution error - fail to donate
 }
 
 func NewActionModule(b1 bool, b2 bool, b3 bool, b4 bool) *ActionModule {
-  return &ActionModule { bits: [4]bool{b1, b2, b3, b4} }
+  return &ActionModule { bits: [4]bool{b1, b2, b3, b4},
+                         errP: 0.001 }
 }
 
 func (self *ActionModule) ChooseDonate(donor Rep, recip Rep) bool {
+  var rval bool
   if (donor == GOOD) {
     if (recip == GOOD) {
-      return self.bits[0]
+      rval = self.bits[0]
     } else {
-      return self.bits[1]
+      rval = self.bits[1]
     }
   } else {
     if (recip == GOOD) {
-      return self.bits[2]
+      rval = self.bits[2]
     } else {
-      return self.bits[3]
+      rval = self.bits[3]
     }
   }
+  // check for execution error
+  if (RandPercent() < float64(self.errP)) {
+    rval = !rval
+  }
+  // return the action
+  return rval
 }
