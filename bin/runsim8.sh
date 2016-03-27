9e0e9e58670877993acede5d23303c789e35fe95
John Maloney <jmmaloney3@gmail.com>
2016-03-26 21:24:32 -0500

    modify script to allow command line arguments to be passed to sim execution program (runsim.sh)

--- bin/runsim8.sh
+++ bin/runsim8.sh
@@ -8,22 +8,42 @@ DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 SOURCE="$(readlink "$SOURCE")"
 [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
+# assumption: this bash script is in the $BIN directory
 BIN="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
 # echo $BIN
 
-# assumption: this bash script is in the $BIN directory
-GO=$BIN
+# get the name to use for the directory that holds results
+RESULTSDIR=$1
+shift # remove the directory name from the list of arguments
 
-# run the Python script to execute simulation
-# assumption: the Python program is in the $PYT directory
-BASE='stats'-$1
-mkdir $BASE
-cd $BASE
-for i in `seq -f "%03g" 1 $2`;
+# make sure the result directory doesn't exist
+if [ -e $RESULTSDIR ];
+then
+  # echo to stderr
+  >&2 echo "file or directory $RESULTSDIR already exist"
+  exit 1
+fi
+
+# run the runsim go program to execute simulation
+# assumption: the go program is in the $BIN directory
+mkdir $RESULTSDIR
+cd $RESULTSDIR
+>&2 echo "  writing results to $RESULTSDIR"
+
+# get the number of simulations to execute
+NUMSIMS=$1
+>&2 echo "  running $NUMSIMS simulations"
+shift # remove the number of sims from list of arguments
+
+# echo the command that will be used to execute each simulation
+>&2 echo "  execute following for each simulation:"
+>&2 echo "  $BIN/runsim $@ -f <num>.csv &> <num>.log"
+
+for i in `seq -f "%03g" 1 $NUMSIMS`;
 do
-  echo "  executing run $i..."
-  $GO/runsim -g $3 -t 64 -a 64 -b 17 -c 1 -beta 10000 -mp -f $i.csv &> ./$i.log
+  >&2 echo "  executing run $i..."
+  $BIN/runsim "$@" -f $i.csv &> $i.log
 done
+
 cd ..
-# python $PYT/runsim.py "$@"
