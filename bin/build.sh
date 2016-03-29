02d5b110e7f6cfd299301a247e44df4a02f56ff8
John Maloney <jmmaloney3@gmail.com>
2016-03-28 21:54:11 -0500

    add scripts to build and clean - should migrate these to a makefile

---
+++ bin/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# find the directory that holds the script
+# - see http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in/246128#246128
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+SOURCE="$(readlink "$SOURCE")"
+[[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+BIN="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+# echo $BIN
+
+# assumption: this bash script is in the $BIN directory
+# set the go path
+GOPATH=$BIN/../go
+
+# build and install the sim package
+go install sim
+
+# run the tests
+go test sim
+
+# build the runsim command and put it in the bin directory
+go build -o $BIN/runsim $GOPATH/src/runsim.go
