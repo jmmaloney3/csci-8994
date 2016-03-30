0c5e4ce1896946157b0fae57ff053d52f0886a87
John Maloney <jmmaloney3@gmail.com>
2016-03-29 21:51:22 -0700

    latest round of sims

--- bin/runsimjobs.sh
+++ bin/runsimjobs.sh
@@ -21,13 +21,28 @@ SIMS=500
 # iterate over values for Beta
 
 # beta = 10^5
-BETA=100000
-time $BIN/runsim8.sh beta-$BETA $SIMS -g $GENS -beta $BETA
+#BETA=100000
+#time $BIN/runsim8.sh beta-$BETA $SIMS -g $GENS -beta $BETA
 
 # beta = 10^3
-BETA=1000
-time $BIN/runsim8.sh beta-$BETA $SIMS -g $GENS -beta $BETA
+#BETA=1000
+#time $BIN/runsim8.sh beta-$BETA $SIMS -g $GENS -beta $BETA
 
 # beta = 10^1
-BETA=10
-time $BIN/runsim8.sh beta-$BETA $SIMS -g $GENS -beta $BETA
+#BETA=10
+#time $BIN/runsim8.sh beta-$BETA $SIMS -g $GENS -beta $BETA
+
+BETA = 1000
+
+# benefit = 5
+BEN=5
+time $BIN/runsim8.sh ben-$BEN $SIMS -g $GENS -b $BEN
+
+# benefit = 10
+BEN=10
+time $BIN/runsim8.sh ben-$BEN $SIMS -g $GENS -b $BEN
+
+# benefit = 20
+BEN=20
+time $BIN/runsim8.sh ben-$BEN $SIMS -g $GENS -b $BEN
+	
