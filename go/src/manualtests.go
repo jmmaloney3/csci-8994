6f66e3e7353b2d0d5762b31c4f97b5089c54ec4a
John Maloney <jmmaloney3@gmail.com>
2016-03-30 22:38:23 -0500

    add manual test cases

---
+++ go/src/manualtests.go
@@ -0,0 +1,11 @@
+package main
+
+import "sim"
+
+func main() {
+  sim.ChooseDonateManualTest()
+
+  sim.AssignRepManualTest()
+
+  sim.SelectParentManualTest()
+}
---
+++ go/src/sim/manualtestutils.go
@@ -0,0 +1,81 @@
+package sim
+
+import "fmt"
+
+func AssignRepManualTest() {
+  fmt.Println("AssessModule.AssignRep")
+  errorRate := float32(0.5)
+
+  // stern judging assessment module
+  am := NewAssessModule(GOOD, BAD, BAD, GOOD, GOOD, BAD, BAD, GOOD, errorRate)
+
+  rnGen := NewRandNumGen()
+
+  // print out some assign rep results
+  errors := 0
+  N := 100
+  for i := 0; i < N; i++ {
+    rep := am.AssignRep(GOOD, GOOD, DONATE, rnGen)
+    if (rep == BAD) { errors++ }
+    //fmt.Printf("rep: %v\n", rep)
+  }
+  fmt.Printf("  expected error rate: %6.4f\n", errorRate)
+  fmt.Printf("  actual error rate:   %6.4f\n", float64(errors)/float64(N))
+}
+
+func ChooseDonateManualTest() {
+  fmt.Println("ActionModule.ChooseDonate")
+  errorRate := float32(0.5)
+
+  // CO action module
+  am := NewActionModule(true, false, true, false, errorRate)
+
+  rnGen := NewRandNumGen()
+
+  // print out some choose donate results
+  errors := 0
+  N := 100
+  for i := 0; i < N; i++ {
+    choice := am.ChooseDonate(GOOD, GOOD, rnGen)
+    if (!choice) { errors++ }
+    //fmt.Printf("choice: %t\n", choice)
+  }
+  fmt.Printf("  expected error rate: %6.4f\n", errorRate)
+  fmt.Printf("  actual error rate:   %6.4f\n", float64(errors)/float64(N))
+}
+
+func SelectParentManualTest() {
+  fmt.Println("Tribe.SelectParent")
+  rnGen := NewRandNumGen()
+  const numAgents = 3
+
+  // tribe with numAgents agents
+  t := NewTribe(numAgents, PASSERR, PACTMUT, PEXEERR, rnGen)
+
+  // set agent's payouts
+  t.agents[0].payout = 10
+  t.agents[1].payout = 20
+  t.agents[2].payout = 30
+
+  // update the tribe total payouts
+  for i := 0; i < numAgents; i++ {
+    t.totalPayouts += t.agents[i].payout
+  }
+
+  // print out some randomly selected parent agents
+  var selected [numAgents]int
+  N := 100
+  for i := 0; i < N; i++ {
+    a := t.SelectParent(rnGen)
+    for j := 0; j < numAgents; j++ {
+      if (a == t.agents[j]) {
+        selected[j]++
+      }
+    }
+    // fmt.Printf("parent payout: %d\n", a.payout)
+  }
+  for i := 0; i < numAgents; i++ {
+    fmt.Printf("  expected rate for agent %d: %6.4f\n", i, float64(t.agents[i].payout)/float64(t.totalPayouts))
+    fmt.Printf("  actual rate for agent   %d: %6.4f\n", i, float64(selected[i])/float64(N))
+  }
+}
