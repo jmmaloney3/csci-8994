3ad2ba731b788318cfb6e03f097c10772b613046
John Maloney <jmmaloney3@gmail.com>
2016-05-10 23:37:56 -0500

    jobs for csel-kh1260-03

--- bin/runsimjobs.sh
+++ bin/runsimjobs.sh
@@ -14,20 +14,20 @@ BIN="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 # run several sim jobs
 
 GENS=10000
-GENS=10
+#GENS=10
 SIMS=500
-SIMS=5
+#SIMS=5
 
-DATE=20160511
+DATE=20160511b
 
 BETA=100
 BEN=8
 
 COUNT=0
-for MUT in 0.00001 0.0001 0.001 0.01 0.1;
+for MUT in 0.001 0.01 0.02 0.03 0.04;
 do
   COUNT=$((COUNT+1))
   DIRNAME=$DATE-$COUNT
-  time $BIN/runsim8.sh $DIRNAME $SIMS -g $GENS -b $BEN -beta $BETA -singdef -passmutall -passmut $MUT
+  time $BIN/runsim8.sh $DIRNAME $SIMS -g $GENS -b $BEN -beta $BETA -singdef -passmutall -pactmut $MUT
   tar czf $DIRNAME.tar.gz $DIRNAME
 done
